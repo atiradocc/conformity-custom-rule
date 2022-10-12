732b3f19a3fd452cc0509e88f329f8d7732f1161
Antonio Tirado <55715751+atiradocc@users.noreply.github.com>
2022-10-12 08:35:52 -0500

    adding the ability to update conditions

--- create-custom-rule.py
+++ create-custom-rule.py
@@ -23,9 +23,9 @@ parser.add_argument('--eventName', type=str, required=True,
 parser.add_argument('--remediationNotes', type=str, required=True,
                     help='Notes or steps relevant to remediation. It can also be a URL')
 parser.add_argument('--service', type=str, required=True,
-                    help='Conformity Cloud Service. See expected values at https://us-west-2.cloudconformity.com/v1/services')
+                    help='Conformity Cloud Service. See https://us-west-2.cloudconformity.com/v1/services')
 parser.add_argument('--resourceType', type=str, required=True,
-                    help='Conformity Resource Type. See expected values at  https://us-west-2.cloudconformity.com/v1/resource-types')
+                    help='Conformity Resource Type. See https://us-west-2.cloudconformity.com/v1/resource-types')
 parser.add_argument('--categories', type=str, nargs="*", required=True, choices=[
                     'security', 'cost-optimisation', 'reliability', 'performance-efficiency', 'operational-excellence', 'sustainability'], help='Best Practice Category Pillars')
 parser.add_argument('--severity', type=str, required=True, choices=[
@@ -35,19 +35,17 @@ parser.add_argument('--provider', type=str, required=True, choices=[
 parser.add_argument('--attributes', type=dictionary, action='append', required=True,
 	help='Collection of user defined attribute names and the associated resource value that will be used as part of the rule logic/evaluation',
 	metavar='name:Attribute Name,path:data.JSON Path,required:True|False')
-
-parser.add_argument('--ruleLogic', type=str, nargs="1", required=True, choices=[
+parser.add_argument('--ruleLogic', type=str, required=True, choices=[
                     'any', 'all'], help='Determine whether ANY or ALL rulesets must pass for a successful check. See https://cloudone.trendmicro.com/docs/conformity/in-preview-custom-rules-overview/#custom-rule-configuration')
 parser.add_argument('--ruleSet', type=dictionary, action='append', required=True,
 	help='Conditions ruleset. See https://cloudone.trendmicro.com/docs/conformity/in-preview-custom-rules-overview/#custom-rule-configuration',
 	metavar='fact:ATTRIBUTENAME,operator:TESTCRITERIA,value:EXPECTEDVALUE')
-parser.add_argument('--ruleSetAny', type=dictionary, action='append',
+parser.add_argument('--rulesetAny', type=dictionary, action='append',
 	help='Additional conditions ruletset evaluated using an ANY operator. See https://cloudone.trendmicro.com/docs/conformity/in-preview-custom-rules-overview/#custom-rule-configuration',
 	metavar='fact:ATTRIBUTENAME,operator:TESTCRITERIA,value:EXPECTEDVALUE')
-parser.add_argument('--ruleSetAll', type=dictionary, action='append',
+parser.add_argument('--rulesetAll', type=dictionary, action='append',
 	help='Additional conditions ruletset evaluated using an ALL operator. See https://cloudone.trendmicro.com/docs/conformity/in-preview-custom-rules-overview/#custom-rule-configuration',
 	metavar='fact:ATTRIBUTENAME,operator:TESTCRITERIA,value:EXPECTEDVALUE')
-
 parser.add_argument('--region', type=str, required=True, choices=[
                     'us-1', 'trend-us-1', 'au-1', 'ie-1', 'sg-1', 'in-1', 'jp-1', 'ca-1', 'de-1'], help='Cloud One Conformity service region')
 parser.add_argument('--apiKey', type=str, required=True,
@@ -68,12 +66,12 @@ payload = {
 	"categories": args.categories,
 	"severity": "{}".format(args.severity),
 	"provider": "{}".format(args.provider),
-	"enabled": True,
+	"enabled": "true",
 	"attributes": args.attributes,
 	"rules": [
 		{
 			"conditions": {
-				args.rootCondition: args.ruleSet
+				"{}".format(args.ruleLogic): args.ruleSet
 			},
 			"event": {
 				"type": "{}".format(args.eventName)
@@ -81,6 +79,12 @@ payload = {
 		}
 	]
 }
+
+if args.rulesetAny is not None:
+	payload["rules"][0]["conditions"][args.ruleLogic].append({ "any": args.rulesetAny })
+
+if args.rulesetAll is not None:
+	payload["rules"][0]["conditions"][args.ruleLogic].append({ "all": args.rulesetAll })
 
 conformityEndpoint = "https://conformity.{}.cloudone.trendmicro.com/api/custom-rules".format(
     args.region)
